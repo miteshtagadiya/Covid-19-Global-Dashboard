94ae905a52ddae51783e32b3e519b8e52e491d9f
mitesh <miteshtagadiya@gmail.com>
2020-03-27 10:28:48 +0530

    Production build

---
+++ build/precache-manifest.44d41e49cc2a4996adac165d87e27c86.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8cd095345f23892d7997ec65b4f090",
+    "url": "/Covid-19-Global-Dashboard/index.html"
+  },
+  {
+    "revision": "e26c6fb0f829aba147f1",
+    "url": "/Covid-19-Global-Dashboard/static/css/2.94a79f0b.chunk.css"
+  },
+  {
+    "revision": "558f065540c47f842397",
+    "url": "/Covid-19-Global-Dashboard/static/css/main.784e524c.chunk.css"
+  },
+  {
+    "revision": "e26c6fb0f829aba147f1",
+    "url": "/Covid-19-Global-Dashboard/static/js/2.081ae251.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/Covid-19-Global-Dashboard/static/js/2.081ae251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558f065540c47f842397",
+    "url": "/Covid-19-Global-Dashboard/static/js/main.5cd8a920.chunk.js"
+  },
+  {
+    "revision": "f042cc377b219b2fd9a3",
+    "url": "/Covid-19-Global-Dashboard/static/js/runtime-main.55d25ab9.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Covid-19-Global-Dashboard/static/media/github1.0ba2aa20.png"
+  },
+  {
+    "revision": "b9bb7cd7405d487aee26fe56c948e074",
+    "url": "/Covid-19-Global-Dashboard/static/media/india.b9bb7cd7.png"
+  },
+  {
+    "revision": "2d7d3b9986ff9fa55bc24d932923d748",
+    "url": "/Covid-19-Global-Dashboard/static/media/virus.2d7d3b99.gif"
+  },
+  {
+    "revision": "5a35664bc3c522599bb28e1cf79cff56",
+    "url": "/Covid-19-Global-Dashboard/static/media/world.5a35664b.png"
+  }
+]);
