fff06c07947bfd485a07c202cb82236da86ee085
mitesh <miteshtagadiya@gmail.com>
2020-03-27 10:23:23 +0530

    Router changed to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorker from "./serviceWorker";
+import ErrorBoundary from "./ErrorBoundry";
+import { Index } from "./Routes";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <HashRouter>
+    <ErrorBoundary>
+      <Index />
+    </ErrorBoundary>
+  </HashRouter>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
