c361a9cdacfc80b66a3e252da848f5808f079453
mitesh <miteshtagadiya@gmail.com>
2020-04-03 00:01:41 +0530

    production build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-19-Global-Dashboard/precache-manifest.8359d34b2e6583f448d0721bf8aeaf22.js"
+  "/Covid-19-Global-Dashboard/precache-manifest.88c0caa15c8925bc717a6c76a4ec32b5.js"
 );
 
 self.addEventListener('message', (event) => {
