3d27751e2eaa450b8adc2dc275cbb639285b2a4e
mitesh <miteshtagadiya@gmail.com>
2020-04-04 19:38:19 +0530

    Window dimention added

--- src/TinyChart.jsx
+++ src/TinyChart.jsx
@@ -6,7 +6,7 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
 } from "recharts";
 
 const data = [
@@ -14,54 +14,66 @@ const data = [
     name: "Page A",
     uv: 4000,
     pv: 2400,
-    amt: 2400
+    amt: 2400,
   },
   {
     name: "Page B",
     uv: 3000,
     pv: 1398,
-    amt: 2210
+    amt: 2210,
   },
   {
     name: "Page C",
     uv: 2000,
     pv: 9800,
-    amt: 2290
+    amt: 2290,
   },
   {
     name: "Page D",
     uv: 2780,
     pv: 3908,
-    amt: 2000
+    amt: 2000,
   },
   {
     name: "Page E",
     uv: 1890,
     pv: 4800,
-    amt: 2181
+    amt: 2181,
   },
   {
     name: "Page F",
     uv: 2390,
     pv: 3800,
-    amt: 2500
+    amt: 2500,
   },
   {
     name: "Page G",
     uv: 3490,
     pv: 4300,
-    amt: 2100
-  }
+    amt: 2100,
+  },
 ];
 
 export default class TinyChart extends PureComponent {
-  static jsfiddleUrl = "https://jsfiddle.net/alidingling/exh283uh/";
+  state = { width: 0, height: 0 };
 
-  
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
 
   render() {
     return (
-      <LineChart width={250} height={50} data={this.props.data}>
+      <LineChart
+        width={this.state.width < 1439 ? 140 : 250}
+        height={this.state.width < 250 ? 30 : 50}
+        data={this.props.data}
+      >
         <Line
           type="monotone"
           dataKey={this.props.label}
