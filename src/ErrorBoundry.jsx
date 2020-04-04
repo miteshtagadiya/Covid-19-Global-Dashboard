c4e289c944a159ef016ccd9cde28a72c6fa5c168
mitesh <miteshtagadiya@gmail.com>
2020-04-04 11:10:30 +0530

    ErrorBoundary changed

--- src/ErrorBoundry.jsx
+++ src/ErrorBoundry.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ErrorBoundry.css";
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -19,7 +20,37 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div>
+          <section className="page_404">
+            <div className="container">
+              <div className="row">
+                <div
+                  className="col-sm-12"
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  <div className="col-sm-10 col-sm-offset-1  text-center">
+                    <div className="four_zero_four_bg"></div>
+
+                    <div className="contant_box_404">
+                      <h3 className="h2">Look like you're lost</h3>
+
+                      <p>the page you are looking for not available!</p>
+
+                      <a
+                        href="https://miteshtagadiya.github.io/Covid-19-Global-Dashboard"
+                        className="link_404"
+                      >
+                        Go to Home
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      );
     }
 
     return this.props.children;
