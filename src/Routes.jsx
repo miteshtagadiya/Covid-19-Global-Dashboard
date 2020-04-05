4cc0e8162d596619b93caca81c3ddd3fe691d921
mitesh <miteshtagadiya@gmail.com>
2020-04-05 22:19:40 +0530

    Browser history fixed

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import India from "./India";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 import App from "./App";
+
+const history = createBrowserHistory();
+
+ReactGA.initialize("UA-162840601-1", { debug: true });
+ReactGA.set({ page: window.location.pathname + window.location.hash }); // Update the user's current page
+ReactGA.pageview(window.location.pathname + window.location.hash);
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname + location.hash }); // Update the user's current page
+  ReactGA.pageview(location.pathname + location.hash); // Record a pageview for the given page
+});
 
 export class Index extends Component {
   render() {
