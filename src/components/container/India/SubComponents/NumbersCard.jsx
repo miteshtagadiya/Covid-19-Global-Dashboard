9e55f0bafca1fdf03ca1e2ff003dea900f6aad09
mitesh <miteshtagadiya@gmail.com>
2020-04-12 16:24:20 +0530

    NumericCard and India Table moved to India/SubComponents

---
+++ src/components/container/India/SubComponents/NumbersCard.jsx
@@ -0,0 +1,443 @@
+import React, { Component } from "react";
+import TinyChart from "components/charts/TinyChart";
+import Popover from "react-popover";
+import ReactGA from "react-ga";
+
+class NumbersCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    let confirmedTinyChartData =
+      this.props.india.length !== 0
+        ? this.props.india.cases_time_series.map((cases) => {
+            return cases.totalconfirmed !== ""
+              ? {
+                  name: cases.date,
+                  confirmed: Number(cases.totalconfirmed),
+                }
+              : null;
+          })
+        : null;
+
+    let activeTinyChartData =
+      this.props.india.length !== 0
+        ? this.props.india.cases_time_series.map((cases) => {
+            return cases.totalconfirmed !== ""
+              ? {
+                  name: cases.date,
+                  confirmed:
+                    Number(cases.totalconfirmed) -
+                    Number(cases.totaldeceased) -
+                    Number(cases.totalrecovered),
+                }
+              : null;
+          })
+        : null;
+    let deathTinyChartData =
+      this.props.india.length !== 0
+        ? this.props.india.cases_time_series.map((cases) => {
+            return cases.totaldeceased !== ""
+              ? {
+                  name: cases.date,
+                  confirmed: Number(cases.totaldeceased),
+                }
+              : null;
+          })
+        : null;
+    let recoveredTinyChartData =
+      this.props.india.length !== 0
+        ? this.props.india.cases_time_series.map((cases) => {
+            return cases.totalrecovered !== ""
+              ? {
+                  name: cases.date,
+                  confirmed: Number(cases.totalrecovered),
+                }
+              : null;
+          })
+        : null;
+
+    const totalConformedToday =
+      this.props.india.length !== 0
+        ? this.props.india.statewise.filter(
+            (record) => record.state === "Total"
+          )[0].deltaconfirmed
+        : 0;
+
+    const totalRecoveredToday =
+      this.props.india.length !== 0
+        ? this.props.india.statewise.filter(
+            (record) => record.state === "Total"
+          )[0].deltarecovered
+        : 0;
+
+    const totalDeathsToday =
+      this.props.india.length !== 0
+        ? this.props.india.statewise.filter(
+            (record) => record.state === "Total"
+          )[0].deltadeaths
+        : 0;
+
+    const confirmedPopover =
+      this.props.india.length !== 0
+        ? this.props.india.statewise
+            .filter((record) => record.state !== "Total")
+            .filter((state) => Number(state.deltaconfirmed) !== 0)
+            .map((state) => {
+              return { [state.state]: state.deltaconfirmed };
+            })
+        : [];
+
+    const recoveredPopover =
+      this.props.india.length !== 0
+        ? this.props.india.statewise
+            .filter((record) => record.state !== "Total")
+            .filter((state) => Number(state.deltarecovered) !== 0)
+            .map((state) => {
+              return { [state.state]: Number(state.deltarecovered) };
+            })
+        : [];
+    const deathsPopover =
+      this.props.india.length !== 0
+        ? this.props.india.statewise
+            .filter((record) => record.state !== "Total")
+            .filter((state) => Number(state.deltadeaths) !== 0)
+            .map((state) => {
+              return { [state.state]: Number(state.deltadeaths) };
+            })
+        : [];
+
+    return (
+      <div className="row" style={{ padding: "50px 0px 30px 0px" }}>
+        <div className="col-sm-3 col-6" style={{ padding: 15 }}>
+          <div
+            style={{
+              padding: 20,
+              fontWeight: "bold",
+              background: "#448AFF",
+              color: "white",
+              borderRadius: 10,
+            }}
+          >
+            <div style={{ fontSize: 30 }}>
+              {this.props.india.length !== 0
+                ? this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].confirmed
+                : 0}
+            </div>
+            <div
+              style={{
+                fontSize: 15,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <span style={{ fontSize: 17, fontWeight: "bold" }}>&#9650;</span>
+              {this.props.india.length !== 0
+                ? typeof this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltaconfirmed !== "undefined" &&
+                  this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltaconfirmed !== null
+                  ? this.props.india.statewise.filter(
+                      (record) => record.state === "Total"
+                    )[0].deltaconfirmed
+                  : 0
+                : 0}
+              {Number(totalConformedToday) !== 0 ? (
+                <Popover
+                  body={
+                    <div
+                      style={{
+                        background: "white",
+                        borderRadius: 10,
+                        padding: 15,
+                        color: "#192a56",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {confirmedPopover.map((state, index) => {
+                        return (
+                          <div key={index}>
+                            {Object.keys(state)[0]}: {Object.values(state)[0]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  }
+                  preferPlace={"below"}
+                  isOpen={this.state.confirmedOpen}
+                  onOuterAction={() =>
+                    this.setState({
+                      confirmedOpen: !this.state.confirmedOpen,
+                    })
+                  }
+                >
+                  <div
+                    onClick={() => {
+                      ReactGA.event({
+                        category: "India Info",
+                        action: "Confirmed Info Clicked",
+                        label: "Confirm Info",
+                      });
+                      this.setState({
+                        confirmedOpen: !this.state.confirmedOpen,
+                      });
+                    }}
+                    className="report-tile"
+                    style={{ marginLeft: 5, cursor: "pointer" }}
+                  >
+                    ?
+                  </div>
+                </Popover>
+              ) : null}
+            </div>
+            <div style={{ fontSize: 18 }}>Confirmed</div>
+            {this.props.india.length !== 0 ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TinyChart
+                  data={confirmedTinyChartData}
+                  label="confirmed"
+                  color="#ffffff"
+                />
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <div className="col-sm-3 col-6" style={{ padding: 15 }}>
+          <div
+            style={{
+              padding: 20,
+              fontWeight: "bold",
+              background: "#F9A825",
+              color: "#ffffff",
+              borderRadius: 10,
+            }}
+          >
+            <div style={{ fontSize: 30, marginBottom: 35 }}>
+              {this.props.india.length !== 0
+                ? this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].active
+                : 0}
+            </div>
+
+            <div style={{ fontSize: 18 }}>Active</div>
+            {this.props.india.length !== 0 ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TinyChart
+                  data={activeTinyChartData}
+                  label="confirmed"
+                  color="#ffffff"
+                />
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <div className="col-sm-3 col-6" style={{ padding: 15 }}>
+          <div
+            style={{
+              padding: 20,
+              fontWeight: "bold",
+              background: "#4CAF50",
+              color: "#ffffff",
+              borderRadius: 10,
+            }}
+          >
+            <div style={{ fontSize: 30 }}>
+              {this.props.india.length !== 0
+                ? this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].recovered
+                : 0}
+            </div>
+            <div
+              style={{
+                fontSize: 15,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <span style={{ fontSize: 17, fontWeight: "bold" }}>&#9650;</span>
+              {this.props.india.length !== 0
+                ? typeof this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltarecovered !== "undefined" &&
+                  this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltarecovered !== null
+                  ? this.props.india.statewise.filter(
+                      (record) => record.state === "Total"
+                    )[0].deltarecovered
+                  : 0
+                : 0}
+              {Number(totalRecoveredToday) !== 0 ? (
+                <Popover
+                  body={
+                    <div
+                      style={{
+                        background: "white",
+                        borderRadius: 10,
+                        padding: 15,
+                        color: "#192a56",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {recoveredPopover.map((state, index) => {
+                        return (
+                          <div key={index}>
+                            {Object.keys(state)[0]}: {Object.values(state)[0]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  }
+                  preferPlace={"below"}
+                  isOpen={this.state.activeOpen}
+                  onOuterAction={() =>
+                    this.setState({
+                      activeOpen: !this.state.activeOpen,
+                    })
+                  }
+                >
+                  <div
+                    onClick={() => {
+                      ReactGA.event({
+                        category: "India Info",
+                        action: "Active Info Clicked",
+                        label: "Active Info",
+                      });
+                      this.setState({
+                        activeOpen: !this.state.activeOpen,
+                      });
+                    }}
+                    className="report-tile"
+                    style={{ marginLeft: 5, cursor: "pointer" }}
+                  >
+                    ?
+                  </div>
+                </Popover>
+              ) : null}
+            </div>
+            <div style={{ fontSize: 18 }}>Recovered</div>
+            {this.props.india.length !== 0 ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TinyChart
+                  data={recoveredTinyChartData}
+                  label="confirmed"
+                  color="#ffffff"
+                />
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <div className="col-sm-3 col-6" style={{ padding: 15 }}>
+          <div
+            style={{
+              padding: 20,
+              fontWeight: "bold",
+              background: "#FF5252",
+              color: "#ffffff",
+              borderRadius: 10,
+            }}
+          >
+            <div style={{ fontSize: 30 }}>
+              {this.props.india.length !== 0
+                ? this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deaths
+                : 0}
+            </div>
+            <div
+              style={{
+                fontSize: 15,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <span style={{ fontSize: 17, fontWeight: "bold" }}>&#9650;</span>
+              {this.props.india.length !== 0
+                ? typeof this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltadeaths !== "undefined" &&
+                  this.props.india.statewise.filter(
+                    (record) => record.state === "Total"
+                  )[0].deltadeaths !== null
+                  ? this.props.india.statewise.filter(
+                      (record) => record.state === "Total"
+                    )[0].deltadeaths
+                  : 0
+                : 0}
+              {Number(totalDeathsToday) !== 0 ? (
+                <Popover
+                  body={
+                    <div
+                      style={{
+                        background: "white",
+                        borderRadius: 10,
+                        padding: 15,
+                        color: "#192a56",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {deathsPopover.map((state, index) => {
+                        return (
+                          <div key={index}>
+                            {Object.keys(state)[0]}: {Object.values(state)[0]}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  }
+                  preferPlace={"below"}
+                  isOpen={this.state.deathsOpen}
+                  onOuterAction={() =>
+                    this.setState({
+                      deathsOpen: !this.state.deathsOpen,
+                    })
+                  }
+                >
+                  <div
+                    onClick={() => {
+                      ReactGA.event({
+                        category: "India Info",
+                        action: "Deaths Info Clicked",
+                        label: "Deaths Info",
+                      });
+                      this.setState({
+                        deathsOpen: !this.state.deathsOpen,
+                      });
+                    }}
+                    className="report-tile"
+                    style={{ marginLeft: 5, cursor: "pointer" }}
+                  >
+                    ?
+                  </div>
+                </Popover>
+              ) : null}
+            </div>
+            <div style={{ fontSize: 18 }}>Deaths</div>
+            {this.props.india.length !== 0 ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TinyChart
+                  data={deathTinyChartData}
+                  label="confirmed"
+                  color="#ffffff"
+                />
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NumbersCard;
---
+++ src/components/container/India/SubComponents/Table.jsx
@@ -0,0 +1,255 @@
+import React, { Component } from "react";
+import ReactTable from "react-table-6";
+import "react-table-6/react-table.css";
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const data =
+      this.props.india.length !== 0
+        ? this.props.india.statewise
+            .filter((state) => state.state !== "Total")
+            .map((state) => {
+              return {
+                state: state.state,
+                confirmed: Number(state.confirmed),
+                active: Number(state.active),
+                recovered: Number(state.recovered),
+                deaths: Number(state.deaths),
+                deltaConfirmed: Number(state.deltaconfirmed),
+                deltaActive: Number(
+                  state.deltaconfirmed -
+                    state.deltarecovered -
+                    state.deltadeaths
+                ),
+                deltaRecovered: Number(state.deltarecovered),
+                deltaDeaths: Number(state.deltadeaths),
+              };
+            })
+        : [];
+
+    const columns =
+      this.props.india.length !== 0
+        ? [
+            {
+              Header: "State/UT",
+              accessor: "state", // String-based value accessors!
+            },
+            {
+              Header: "Confirmed",
+              accessor: "confirmed",
+              Cell: (props) => {
+                return (
+                  <>
+                    {props.original.deltaConfirmed === 0 ? null : (
+                      <span
+                        style={{
+                          fontSize: 15,
+                          fontWeight: "bold",
+                          color: "#e43339",
+                        }}
+                      >
+                        <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                          &#9650;
+                        </span>
+                        {props.original.deltaConfirmed}
+                      </span>
+                    )}{" "}
+                    <span className="number">{props.value}</span>
+                  </>
+                );
+              },
+            },
+            {
+              Header: "Active",
+              accessor: "active",
+              Cell: (props) => {
+                return (
+                  <>
+                    {props.original.deltaActive === 0 ? null : (
+                      <span
+                        style={{
+                          fontSize: 15,
+                          fontWeight: "bold",
+                          color: "#192a56",
+                        }}
+                      >
+                        <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                          &#9650;
+                        </span>
+                        {props.original.deltaActive}
+                      </span>
+                    )}{" "}
+                    <span className="number">{props.value}</span>
+                  </>
+                );
+              },
+            },
+            {
+              Header: "Recovered",
+              accessor: "recovered",
+              Cell: (props) => {
+                return (
+                  <>
+                    {props.original.deltaRecovered === 0 ? null : (
+                      <span
+                        style={{
+                          fontSize: 15,
+                          fontWeight: "bold",
+                          color: "#006266",
+                        }}
+                      >
+                        <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                          &#9650;
+                        </span>
+                        {props.original.deltaRecovered}
+                      </span>
+                    )}{" "}
+                    <span className="number">{props.value}</span>
+                  </>
+                );
+              },
+            },
+            {
+              Header: "Deaths",
+              accessor: "deaths",
+              Cell: (props) => {
+                return (
+                  <>
+                    {props.original.deltaDeaths === 0 ? null : (
+                      <span
+                        style={{
+                          fontSize: 15,
+                          fontWeight: "bold",
+                          color: "#535c68",
+                        }}
+                      >
+                        <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                          &#9650;
+                        </span>
+                        {props.original.deltaDeaths}
+                      </span>
+                    )}{" "}
+                    <span className="number">{props.value}</span>
+                  </>
+                );
+              },
+            },
+          ]
+        : [];
+
+    const cityData =
+      this.props.stateWiseCity.length !== 0
+        ? Object.keys(this.props.stateWiseCity).map((city) => {
+            return {
+              [city]: Object.keys(
+                this.props.stateWiseCity[city].districtData
+              ).map((key) => {
+                return {
+                  city: key,
+                  confirmed: this.props.stateWiseCity[city].districtData[key]
+                    .confirmed,
+                  active: this.props.stateWiseCity[city].districtData[key]
+                    .active,
+                  recovered: this.props.stateWiseCity[city].districtData[key]
+                    .recovered,
+                  deaths: this.props.stateWiseCity[city].districtData[key]
+                    .deaths,
+                };
+              }),
+            };
+          })
+        : [];
+
+    const cityColumns =
+      this.props.india.length !== 0
+        ? [
+            {
+              Header: "District",
+              accessor: "city",
+            },
+            {
+              Header: "Confirmed",
+              accessor: "confirmed",
+            },
+            {
+              Header: "Active",
+              accessor: "active",
+            },
+            {
+              Header: "Recovered",
+              accessor: "recovered",
+            },
+            {
+              Header: "Deaths",
+              accessor: "deaths",
+            },
+          ]
+        : [];
+    return (
+      <div className="row" style={{ marginTop: 15 }}>
+        <div className="col-sm-12">
+          <div
+            style={{
+              background: "white",
+              color: "#404b69",
+              borderRadius: 10,
+            }}
+          >
+            <ReactTable
+              data={data}
+              columns={columns}
+              defaultPageSize={10}
+              className="-striped -highlight"
+              noDataText={<b>No data found</b>}
+              getTheadProps={(state, rowInfo, column) => {
+                return {
+                  style: {
+                    fontWeight: "bold",
+                  },
+                };
+              }}
+              getTdProps={(state, rowInfo, column, instance) => {
+                return {
+                  onClick: (e, handleOriginal) => {
+                    if (handleOriginal) {
+                      handleOriginal();
+                    }
+                  },
+                };
+              }}
+              filterable
+              SubComponent={(row) => {
+                return row.original.confirmed === 0 ? null : (
+                  <div style={{ padding: "20px" }}>
+                    <div style={{ borderRadius: 10 }}>
+                      <ReactTable
+                        data={
+                          Object.values(
+                            cityData.filter(
+                              (state) =>
+                                Object.keys(state)[0] === row.original.state
+                            )[0]
+                          )[0]
+                        }
+                        columns={cityColumns}
+                        defaultPageSize={5}
+                        showPagination={true}
+                      />
+                    </div>
+                  </div>
+                );
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Table;
