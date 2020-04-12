a9bd828c7931feda860082ed2a571b6258a575d7
mitesh <miteshtagadiya@gmail.com>
2020-04-12 16:25:43 +0530

    GradientCardTitle component created in components/ui

---
+++ src/components/ui/GradientCardTitle/GradientCardTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GradientCardTitle.sass";
+
+const GradientCardTitle = (props) => {
+  return <div className="GradientCardTitle">{props.title}</div>;
+};
+
+export default GradientCardTitle;
