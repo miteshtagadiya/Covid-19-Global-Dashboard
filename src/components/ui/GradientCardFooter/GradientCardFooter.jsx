db0d87225ffed75b06828545b8a887fa67c398de
mitesh <miteshtagadiya@gmail.com>
2020-04-12 16:25:33 +0530

    GradientCardFooter component created in components/ui

---
+++ src/components/ui/GradientCardFooter/GradientCardFooter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GradientCardFooter.sass";
+
+const GradientCardFooter = (props) => {
+  return <div className="GradientCardFooter">{props.content}</div>;
+};
+
+export default GradientCardFooter;
