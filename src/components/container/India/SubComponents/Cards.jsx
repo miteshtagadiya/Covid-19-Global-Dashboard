f2f5d59265acb26b7c0d1e49d5b16b6c7839eb6d
mitesh <miteshtagadiya@gmail.com>
2020-04-14 22:12:55 +0530

    State chart added in card ui

--- src/components/container/India/SubComponents/Cards.jsx
+++ src/components/container/India/SubComponents/Cards.jsx
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
+import SimpleLineChart from "components/charts/SimpleLineChart/SimpleLineChart";
+import SwitchBar from "components/ui/SwitchBar/SwitchBar";
 import Virus from "assets/virus.gif";
 
 class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      changeChart: "Confirmed",
+    };
+  }
+
   render() {
+    let colors = {
+      Confirmed: "#e43339",
+      Recovered: "#006266",
+      Deaths: "#535c68",
+    };
+
+    let dailyConfirmedStatus = this.props.dailyStatus.filter(
+      (data) => data.status === "Confirmed"
+    );
+    let dailyRecoveredStatus = this.props.dailyStatus.filter(
+      (data) => data.status === "Recovered"
+    );
+    let dailyDeathsStatus = this.props.dailyStatus.filter(
+      (data) => data.status === "Deceased"
+    );
+
+    let count = (code, data, index) => {
+      let confirmed = 0;
+      for (var i = 0; i <= index; i++) {
+        confirmed += Number(data[i][code]);
+      }
+      return confirmed;
+    };
+
     return (
       <div className="row">
         {this.props.locationLoader === true ? (
@@ -17,6 +50,27 @@ class Cards extends Component {
                 .includes(this.props.searchString.toLowerCase())
             )
             .map((location, index) => {
+              let f = dailyConfirmedStatus.map((data1, index) => {
+                return {
+                  name: data1["date"],
+                  Confirmed: count(
+                    location.statecode.toLowerCase(),
+                    dailyConfirmedStatus,
+                    index
+                  ),
+                  Recovered: count(
+                    location.statecode.toLowerCase(),
+                    dailyRecoveredStatus,
+                    index
+                  ),
+                  Deaths: count(
+                    location.statecode.toLowerCase(),
+                    dailyDeathsStatus,
+                    index
+                  ),
+                };
+              });
+
               return location.state !== "Total" ? (
                 <div key={index} className="col-sm-4" style={{ padding: 15 }}>
                   <div
@@ -48,20 +102,79 @@ class Cards extends Component {
                     >
                       <div style={{ padding: 10, fontWeight: "bold" }}>
                         <div>{location.confirmed}</div>
+                        <div
+                          style={{
+                            fontSize: 15,
+                            fontWeight: "bold",
+                            color: "#e43339",
+                          }}
+                        >
+                          <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                            &#9650;
+                          </span>
+                          {location.deltaconfirmed}
+                        </div>
                         <div>Confirmed</div>
                       </div>
                       <div style={{ padding: 10, fontWeight: "bold" }}>
-                        <div>{location.active}</div>
+                        <div style={{ marginBottom: 22 }}>
+                          {location.active}
+                        </div>
                         <div>Active</div>
                       </div>
                       <div style={{ padding: 10, fontWeight: "bold" }}>
                         <div>{location.recovered}</div>
+                        <div
+                          style={{
+                            fontSize: 15,
+                            fontWeight: "bold",
+                            color: "#006266",
+                          }}
+                        >
+                          <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                            &#9650;
+                          </span>
+                          {location.deltarecovered}
+                        </div>
+
                         <div>Recovered</div>
                       </div>
                       <div style={{ padding: 10, fontWeight: "bold" }}>
                         <div>{location.deaths}</div>
+                        <div
+                          style={{
+                            fontSize: 15,
+                            fontWeight: "bold",
+                            color: "#535c68",
+                          }}
+                        >
+                          <span style={{ fontSize: 15, fontWeight: "bold" }}>
+                            &#9650;
+                          </span>
+                          {location.deltadeaths}
+                        </div>
                         <div>Deaths</div>
                       </div>
+                    </div>
+                    <div>
+                      <SwitchBar
+                        options={["Conf.", "Recv.", "Deaths"]}
+                        optionValues={["Confirmed", "Recovered", "Deaths"]}
+                        onClick={(prop) => {
+                          this.setState({
+                            changeChart: prop,
+                          });
+                        }}
+                        showGlobalChartType={this.state.changeChart}
+                      />
+                      <SimpleLineChart
+                        chart={"AreaChart"}
+                        customTooltip={true}
+                        grid={false}
+                        data={f}
+                        labels={[this.state.changeChart]}
+                        colors={[colors[this.state.changeChart]]}
+                      />
                     </div>
                   </div>
                 </div>
